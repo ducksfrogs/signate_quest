47823b0d53975b032ae9d9119134db7594f9763a
ma <ma_yamaki@yahoo.com>
2020-12-05 18:21:53 +0900

    ddfa

--- sport_ticket/code/momon.py
+++ sport_ticket/code/momon.py
@@ -4,3 +4,9 @@ counts.plot.bar(title="stageの頻度")
 
 plt.xlabel("stage")
 plt.ylabel("count")
+
+import seaborn as sns
+
+sns.boxplot(data=data[data['stage']=='Ｊ１'], x='mathch_num', y='y')
+sns.boxplot(data=data[data['stage']=='Ｊ１'], x='mathch_num', y='y')
+sns.boxplot

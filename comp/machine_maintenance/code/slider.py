d0059417a1062fd304b0ab93dd9f0530c3a4001d
ducksfrogs <ma_yamaki@yahoo.com>
2021-01-06 12:37:19 +0900

    d'

--- comp/machine_maintenance/code/slider.py
+++ comp/machine_maintenance/code/slider.py
@@ -16,7 +16,7 @@ def read_data(category, train_test, label):
         dataset.append(y)
     return np.array(dataset)
 
-    
+
 train_normal = read_data('slider', 'train', 'normal')
 train_anomaly = read_data('slider', 'train', 'anomaly')
 
@@ -27,4 +27,21 @@ train_df['mean'] = np.sqrt(np.mean(train**2, axis=1))
 train_df['zc'] = np.sum(librosa.zero_crossings(train), axis=1)
 train_df['label'] = np.concatenate([np.zeros(len(train_normal)),np.ones(len(train_anomaly))])
 
-test
+test_normal = read_data('slider', 'test', 'normal')
+test_anomaly = read_data('slider', 'test', 'anomaly')
+
+test = np.concatenate([test_normal, test_anomaly])
+test_df = pd.DataFrame()
+test_df['mean'] = np.sqrt(np.mean(test**2, axis=1))
+test_df['zc'] = np.sum(librosa.zero_crossings(test), axis=1)
+test_df['label'] = np.concatenate([np.zeros(len(test_normal)), np.ones(len(test_anomaly))])
+
+
+ train_x, train_y = train_df[['mean','zc']], train_df]['label']
+ test_x, test_y = test_df[['mean','zc']], test_df['label']
+
+ model.fit(train_x, train_y)
+
+ pred = model.predict(test_x)
+
+ confusion_matrix(test_y, pred)

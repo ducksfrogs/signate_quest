3999a17ccba748a43c7d2421e5d0ce57989ea95a
ducksfrogs <ma_yamaki@yahoo.com>
2021-01-06 09:57:10 +0900

    dd

--- comp/machine_maintenance/code/valve_ex.py
+++ comp/machine_maintenance/code/valve_ex.py
@@ -1,4 +1,29 @@
 from sklearn.ensemble import RandomForestClassifier
 import librosa
 
+import glob
+
 import numpy as np
+import pandas as pd
+
+def read_data(categorie):
+    files = sorted(glob.glob(f"../input/{categorie}/*.wav"))
+    dataset = []
+    for file_name in files:
+        y, sr = librosa.load(file_name, sr=None)
+        dataset.append(y)
+
+    return np.array(dataset)
+
+normals = read_data('valid_normal')
+anomaly = read_data('valid_anomaly')
+
+train = np.concatenate([normals,anomaly])
+
+train_mean = np.sqrt(np.mean(train**2, axis=1))
+train_zc = np.sum(librosa.zero_crossings(train), axis=1)
+
+train_df = pd.DataFrame()
+train_df['mean'] = train_mean
+train_df['zc'] = train_zc
+train_df['label'] = np.concatenate([np.zeros(len(normals)), np.ones(len(anomaly))])

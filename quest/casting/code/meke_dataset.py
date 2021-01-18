95150111afe658293e9f2e338ec6a0989d5aca14
ma <ma_yamaki@yahoo.com>
2021-01-18 14:12:04 +0900

    d

--- quest/casting/code/meke_dataset.py
+++ quest/casting/code/meke_dataset.py
@@ -1,13 +1,29 @@
+from torchvision import transforms, datasets
+import zipfile
 
-from torchvision import transforms
+def unzip_dataset(INPATH, OUTPATH):
+    with zipfile.ZipFile(INPATH) as zf:
+        zf.extractall(OUTPATH)
+
+unzip_dataset(INPATH='./image_data.zip', OUTPATH='./')
+
+
 
 data_transforms = {
     'train':transforms.Compose([
         transforms.Resize(256),
+        transforms.RandomHorizontalFlip(p=0.5),
         transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [.229, 0.224, 0.225]),
     ]),
     'val': transforms.Compose([
         transforms.Resize(256),
         transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [.229, 0.224, 0.225]),
     ]),
+}
+
+image_datasets = {
+    'train': datasets.ImageFolder('./image_data/train', data_transforms['train']),
+    'val': datasets.ImageFolder('./image_data/val', data_transforms['val'])
 }

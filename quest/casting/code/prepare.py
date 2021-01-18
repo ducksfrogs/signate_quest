d73cb1f33c36fc87fea28bdc0314e9bd6c1545ff
ma <ma_yamaki@yahoo.com>
2021-01-18 19:16:48 +0900

    dd

--- quest/casting/code/prepare.py
+++ quest/casting/code/prepare.py
@@ -6,25 +6,24 @@ import shutil
 os.makedirs(dst_dir, exist_ok=True)
 
 src_dir = 'images'
-dst_dir = 'output'
+dst_dir = './input/'
 
-ok_path = "./ok"
-ng_path = "./ng"
+ok_path = "/ok"
+ng_path = "/ng"
 
 data_file = glob.glob(os.path.join(src_dir, '*.jpeg'))
 
-ok_path = './input/'
 for img in data_file:
     filename = os.path.basename(img)
     if 'ok' in filename:
         dst = os.path.join(ok)
 
-train_val = ['', '']
+train_val = ['train', 'val']
 
 
 
 for i in train_val:
-    tmp_path = './input/{}'.format(i)
+    tmp_path = './input/{}_data/'.format(i)
     data_file = glob.glob(os.path.join(tmp_path, '*.jpeg'))
     for img in data_file:
         filename = os.path.basename(img)
@@ -34,5 +33,3 @@ for i in train_val:
         else:
             dst = os.path.join(ng_path, filename)
             shutil.copy2(img, dst)
-
-            

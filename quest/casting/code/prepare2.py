01f1e21289c9a42288e179a16e58782994259149
ducksfrogs <ma_yamaki@yahoo.com>
2021-02-06 17:13:24 +0900

    j

--- quest/casting/code/prepare2.py
+++ quest/casting/code/prepare2.py
@@ -16,10 +16,28 @@ print(len(ng_image_name_list))
 print(len(set(ok_image_name_list)))
 print(len(set(ng_image_name_list)))
 
-from torchvision import transforms
+from torchvision import transforms, datasets
 
-data_transform ={
+data_transforms ={
     'train': transforms.Compose([
-        transforms.Resize(256)
+        transforms.Resize(256),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
+}
+
+image_datasets = {
+    'train': datasets.ImageFolder('./image_data/train', data_transforms['train']),
+    'val': datasets.ImageFolder("./image_data/val", data_transforms['val'])
+}
+
+image_dataloaders = {
+ 'train': torch.utils.data.DataLoader(image_datasets['train'], batch_size=4, num_workers=0, drop_last=True)
+ 'val': torch.utils.data.DataLoader(image_datasets['val'], batch_size=4, num_workers=0, drop_last=True)
 }

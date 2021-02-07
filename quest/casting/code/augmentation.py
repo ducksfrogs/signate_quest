8746e30e5a7e27dcdc486c6401d76c4735736064
ma <ma_yamaki@yahoo.com>
2021-02-07 15:10:29 +0900

    d

--- quest/casting/code/augmentation.py
+++ quest/casting/code/augmentation.py
@@ -1,8 +1,11 @@
 import zipfile
 import torch
 import matplotlib.pyplot as plt
+import albumentations
 from PIL import Image
 from torchvision import transforms, datasets
+import numpy as np
+
 
 
 def unzip_dataset(INPATH, OUTPATH):
@@ -11,6 +14,28 @@ def unzip_dataset(INPATH, OUTPATH):
 
 unzip_dataset(INPATH='./augment_data.zip', OUTPATH='./')
 
+#transform = transforms.Compose([
+#        transforms.RondomHorizontalFlip(p=0.5),
+#        transforms.RandomVerticalFlip(p=0.5),
+#])
+#aug_dataset = datasets.ImageFolder(root='./augment_data/', transform=transform)
+#show_augment(aug_dataset)
+
+albu_transforms = albumentations.Compose([
+                    albumentations.RandomGridShuffle(grid=(1,1),p=1.0)
+])
+
+def albumentations_transform(image, transform=albu_transforms):
+    image_np = np.array(image)
+    augmented = transform(image=image_np)
+    image = Image.fromarray(augmented['image'])
+    return image
+
+data_transform = transforms.Compose([
+                    transforms.Lambda(albumentations_transform),
+])
+
+dataset_augmentated = datasets.ImageFolder(root='./augment_data/', transform=data_transform)
 
 def show_augment(dataset):
     for i in range(0,6):
@@ -20,9 +45,4 @@ def show_augment(dataset):
         plt.imshow(dataset[i][0])
     plt.show()
 
-transform = transforms.Compose([
-        transforms.RondomHorizontalFlip(p=0.5),
-        transforms.RandomVerticalFlip(p=0.5),
-])
-aug_dataset = datasets.ImageFolder(root='./augment_data/', transform=transform)
-show_augment(aug_dataset)
+show_augment(dataset_augmentated)

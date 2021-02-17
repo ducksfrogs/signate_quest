321c32ae0ac172cb648e49f051e39e115124dde5
ducksfrogs <ma_yamaki@yahoo.com>
2021-02-17 16:49:53 +0900

    tukareta

--- comp/casting_images/code/first.py
+++ comp/casting_images/code/first.py
@@ -23,3 +23,39 @@ for n in range(3):
     image_path = random.choice(all_image_paths)
     display.display(display.Image(image_path))
     print(caption_image(image_path))
+
+
+label_names = ['ok', 'def']
+
+label_to_index = dict((name, index) for index, name in enumerate(label_names))
+
+
+def mk_idx(x):
+    if 'ok' in x:
+        return 'ok'
+    else:
+        return 'def'
+
+all_image_labels = [label_to_index[mk_idx(path)] for path in all_image_paths]
+
+img_path =all_image_paths[0]
+
+img_raw = tf.io.read_file(image_path)
+img_tensor = tf.image.decode_image(img_raw)
+
+img_final = tf.image.resize(img_tensor, [192,192])
+img_final = img_final/255.0
+print(img_final.shape)
+print(img_final.numpy().min())
+print(img_final.numpy().max())
+
+def preprocess_image(image):
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [192,192])
+    image /= 255.0
+
+    return image
+
+def load_and_preprocess_image(path):
+    image = tf.io.read_file(path)
+    return preprocess_image(image)
